b7497b8977f02d44595ed9dbec59f273cd5baeb8
abhishekabhizz <abhishekabhiizz466@gmail.com>
2025-01-15 11:21:58 +0530

    Complete AddTask Functionality

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
 }
   return (
     <div className=" h-screen flex flex-col items-center pt-28 bg-gradient-to-r from-blue-900 from-10% via-violet-500 via-30% to-yellow-400 to-90%">
-      <h1 className="text-3xl font-bold">TO-Do</h1>
+      <h1 className="text-3xl font-bold underline">TO-Do</h1>
       <form className="flex gap-5 mt-10">
         <input value={newTask} onChange={(e)=>{setnewTask(e.target.value)}} className=" border-2 border-white px-4 py-2  outline-none rounded-md " type="text" placeholder="Enter the Task" />
         <button onClick={addTask} className="p-2 border-2 border-white rounded-md font-semibold bg-white text-sm">
